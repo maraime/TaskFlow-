9e35038a11da37f063848e19eee9c6a285adf885
maraim <maryamad493@gmail.com>
2026-05-15 18:12:27 +0100

    feat: implement JWT authentication

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 
@@ -7,29 +8,64 @@ app.use(express.json());
 const users = [];
 
 // Register
-app.post("/api/auth/register", (req, res) => {
-  const user = req.body;
+app.post("/api/auth/register", async (req, res) => {
+  const { email, password, fullName } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = {
+    fullName,
+    email,
+    password: hashedPassword
+  };
+
   users.push(user);
-  
-  res.json({ message: "User registered (fake DB)", user });
+
+  res.json({
+    message: "User registered successfully",
+    user
+  });
 });
 
 // Home
 app.get("/", (req, res) => {
   res.send("TaskFlow backend is working 🚀");
 });
-app.post("/api/auth/login", (req, res) => {
+app.post("/api/auth/login", async (req, res) => {
   const { email, password } = req.body;
 
   const user = users.find(
-    u => u.email === email && u.password === password
+    u => u.email === email
   );
 
   if (!user) {
-    return res.status(400).json({ message: "Invalid credentials" });
+    return res.status(400).json({
+      message: "Invalid credentials"
+    });
   }
 
-  res.json({ message: "Login successful", user });
+  const isMatch = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!isMatch) {
+    return res.status(400).json({
+      message: "Invalid credentials"
+    });
+  }
+
+  const token = jwt.sign(
+    { email: user.email },
+    "mySecretKey",
+    { expiresIn: "1h" }
+  );
+
+  res.json({
+    message: "Login successful",
+    token,
+    user
+  });
 });
 // Start server
 app.listen(3000, () => {
